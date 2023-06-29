49e6b9fd4261c2007d54cac2890d1f9942d06786
Sergei-4515 <ru4515@yandex.ru>
2023-06-29 23:03:48 +0300

    D16.4
    D16.4

--- NewsPaper/news/admin.py
+++ NewsPaper/news/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Post, Category
 
+# создаём новый класс для представления товаров в админке
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'text')
+    list_filter = ('date_in')
+    search_fields = ('name', 'category__name')
+
 
 admin.site.register(Post)
 admin.site.register(Category)
